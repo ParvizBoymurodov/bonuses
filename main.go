49bdbe55f1ab509042a2231fa723228aca7a2927
Parviz Boymurodov <parvizboymurodov@mail.ru>
2020-01-08 14:50:26 +0300

    issue-#1:fixed

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	sales:=[]int{12_000,8_000,15_000,9_000}
-	fmt.Println(bonuses(sales))
 
 }
-func bonuses(sales[] int) int{
-	sum:=0
-	const procent=5
-	const minAmountForBonus =10_000
-	for _, sale:= range sales{
+func bonuses(sales [] int) int {
+	sum := 0
+	const percent = 5
+	const minAmountForBonus = 10_000
+	for _, sale := range sales {
 		if sale > minAmountForBonus {
-			result:=(sale- minAmountForBonus)*procent/100
-			sum=sum+result
+			result := (sale - minAmountForBonus) * percent / 100
+			sum = sum + result
 		}
 	}
 	return sum
